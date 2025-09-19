cd71676721550fbf1c428c00afcfaf570d0fa95c
vrogeon <vrogeon@gmail.com>
2025-09-19 15:52:42 +0200

    Fix issues for files download

--- Repartition.py
+++ Repartition.py
@@ -305,7 +305,7 @@ class Repartition:
     # This function create files for repartition keys
     def write_repartition_key(self, prod_list, cons_list, folder, debug_info = False):
         for index_prod, prod in enumerate(prod_list):
-            file = folder + '/' + prod.name + '_key.csv'
+            file = folder + '/' + prod.name.replace(" ", "") + '_key.csv'
             with open(file, 'w', newline='') as csvfile:
                 keywriter = csv.writer(csvfile,delimiter=';')
 
@@ -381,7 +381,7 @@ class Repartition:
         file_list = []
 
         for index_prod, prod in enumerate(prod_list):
-            file = folder + '/' + prod.name + '_statistics.csv'
+            file = folder + '/' + prod.name.replace(" ", "") + '_statistics.csv'
             file_list.append(file)
             with open(file, 'w', newline='') as csvfile:
                 keywriter = csv.writer(csvfile,delimiter=';')
@@ -450,7 +450,7 @@ class Repartition:
                                 add_auto_prod_rate = True,
                                 add_auto_cons_mois = True):
         for index_prod, prod in enumerate(prod_list):
-            file = folder + '/' + prod.name + '_monthly_report.csv'
+            file = folder + '/' + prod.name.replace(" ", "") + '_monthly_report.csv'
             with open(file, 'w', newline='') as csvfile:
                 keywriter = csv.writer(csvfile,delimiter=';')
 
